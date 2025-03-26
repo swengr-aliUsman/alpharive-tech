f69b610c5113b6ca84d1fe71349bc93bc0057b0a
Ali Usman <aliusman.engr115@gmail.com>
2025-03-26 19:12:25 +0500

    css issue fixed

--- inc/head.php
+++ inc/head.php
@@ -2,13 +2,15 @@
 <link rel="stylesheet" type="text/css" href="assets/css/header.css" />
 <link rel="stylesheet" type="text/css" href="assets/css/custom.css" fetchpriority="low" />
 <link rel="preload" href="assets/css/footer.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
+
 <link rel="preload" href="assets/css/media.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
+<noscript><link rel="stylesheet" href="assets/css/media.css"></noscript>
 
 
 <!-- included in 71 file -->
 <link rel="preload" href="<?php echo $siteurl; ?>css/contact.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
 <link rel="preload" href="<?php echo $siteurl; ?>css/form.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
-<link rel="stylesheet" href="<?php echo $siteurl; ?>assetsindex/css/fontawesome/css/all.min.css" fetchpriority="low">
+<link rel="preload" href="<?php echo $siteurl; ?>assetsindex/css/fontawesome/css/all.min.css" onload="this.onload=null;this.rel='stylesheet'">
 
 
 <!-- included in 61 files -->
