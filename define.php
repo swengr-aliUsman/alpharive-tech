58ca254442ec194e9b94978f4d3061adaaa96fdb
Ali Usman <aliusman.engr115@gmail.com>
2025-04-25 23:35:12 +0500

    version

--- define.php
+++ define.php
@@ -3,7 +3,7 @@
 $siteurl = "https://opris.tokenloopz.com/";
 // $siteurl = "http://localhost/github/alpharive-tech/";
 $canonical = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-$version = '?v=1.78';
+$version = '?v=1.79';
 
 function isMobile() {
     return preg_match('/(iphone|ipod|ipad|android|blackberry|windows phone|opera mini|mobile)/i', $_SERVER['HTTP_USER_AGENT']);
